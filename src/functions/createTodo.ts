a8cc0d3dc706f68711befe536d43c88c637785f2
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-27 15:17:03 -0300

    refatorando returnError

--- src/functions/createTodo.ts
+++ src/functions/createTodo.ts
@@ -1,37 +1,26 @@
 import { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
 import { v4 as uuidV4, validate } from 'uuid'
+import { returnError } from 'src/utils/returnError';
 import { document } from '../utils/dynamodbClient';
+
 
 interface ICreateTodo {
   title: string;
   deadline: string;
 }
 
-const returnError = (statusCode: number, errorMessage: string): APIGatewayProxyResult => {
-  return {
-    statusCode,
-    body: JSON.stringify({
-      error: errorMessage,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    }
-  }
-}
-
-
 export const handle: APIGatewayProxyHandler = async (event) => {
   const { userId } = event.pathParameters;
   if (!validate(userId)) {
-    return returnError(400, 'User ID must be a valid uuid');
+    return returnError('User ID must be a valid uuid');
   }
 
   const { title, deadline } = JSON.parse(event.body) as ICreateTodo;
   if (!title) {
-    return returnError(400, 'Title must be provided');
+    return returnError('Title must be provided');
   }
   if (!deadline) {
-    return returnError(400, 'Deadline must be provided');
+    return returnError('Deadline must be provided');
   }
 
   const id = uuidV4();
