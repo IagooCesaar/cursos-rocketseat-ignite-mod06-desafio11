fce6d69353572283152ac285f9aea3473276bcf4
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-27 16:39:56 -0300

    consultando todos de usuário

--- src/functions/getTodos.ts
+++ src/functions/getTodos.ts
@@ -1,12 +1,31 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { v4 as uuidV4, validate } from 'uuid';
+
+import { returnError } from 'src/utils/returnError';
+import { document } from '../utils/dynamodbClient';
 
 export const handle: APIGatewayProxyHandler = async (event) => {
+  const { userId } = event.pathParameters;
+  if (!validate(userId)) {
+    return returnError('User ID must be a valid uuid');
+  }
+
+  const response = await document.query({
+    TableName: 'todos',
+    IndexName: 'TodoByUserId',
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeValues: {
+      ":userId": {"S": userId},
+    },
+    ScanIndexForward: false,
+    ProjectionExpression: 'title, done, deadline',
+  }).promise();
+
+  const todos = response.Items[0];
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'ok',
-    }),
+    body: JSON.stringify(todos),
     headers: {
       "Content-Type": "application/json",
     }
