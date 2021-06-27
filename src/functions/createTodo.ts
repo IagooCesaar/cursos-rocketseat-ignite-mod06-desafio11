5f189a69113ffcd75ab8164f4fab70c12df6be51
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-27 15:01:45 -0300

    validando userId com uuid

--- src/functions/createTodo.ts
+++ src/functions/createTodo.ts
@@ -1,6 +1,19 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
 export const handle: APIGatewayProxyHandler = async (event) => {
+  const { userId } = event.pathParameters;
+  if (!validate(userId)) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: 'User ID must be a valid uuid',
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }
+  }
+
 
   return {
     statusCode: 201,
