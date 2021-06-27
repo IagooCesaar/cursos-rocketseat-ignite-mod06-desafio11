7ecdb499e203b113b8eee1b9f55283b7d8b1e695
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-27 15:09:04 -0300

    validando campos do todo

--- src/functions/createTodo.ts
+++ src/functions/createTodo.ts
@@ -1,18 +1,39 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import { v4 as uuidV4, validate } from 'uuid'
+
+interface ICreateTodo {
+  title: string;
+  deadline: string;
+}
+
+const returnError = (statusCode: number, errorMessage: string): APIGatewayProxyResult => {
+  return {
+    statusCode,
+    body: JSON.stringify({
+      error: errorMessage,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    }
+  }
+}
+
 
 export const handle: APIGatewayProxyHandler = async (event) => {
   const { userId } = event.pathParameters;
   if (!validate(userId)) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        error: 'User ID must be a valid uuid',
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      }
-    }
+    return returnError(400, 'User ID must be a valid uuid');
   }
+
+  const { title, deadline } = JSON.parse(event.body) as ICreateTodo;
+  if (!title) {
+    return returnError(400, 'Title must be provided');
+  }
+  if (!deadline) {
+    return returnError(400, 'Deadline must be provided');
+  }
+  
+  const id = uuidV4();
 
 
   return {
